f005fd78e7445c41d1d2f288b784be5d5f0c83b7
Joshua <joshaw66@gmail.com>
2020-01-24 17:57:41 -0700

    finally solved Request 2 map after spelling lowCaseAnimalNames wrong multiple times

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -151,7 +151,10 @@ The zoos need a list of all their animal's names (animal_name only) converted to
 
 */
 
-const lowCaseAnimalNames = [];
+const lowCaseAnimalNames = zooAnimals.map(function(currentValue) {
+  return currentValue.animal_name.toLowerCase();
+});
+
 console.log(lowCaseAnimalNames);
 
 /* Request 3: .filter() 
